f88e54b8244be51850bbca60d28a049478cc7fe3
kyvv08 <ky4995@naver.com>
2025-04-21 16:39:21 +0900

    아이템-아이템 매니저 클래스

---
+++ TextConsoleRPG/Item.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TextConsoleRPG
+{
+    readonly struct ItemStatus
+    {
+        public int atk { get; init; }
+        public int def { get; init; }
+        public int health { get; init; }
+    }
+    public enum ViewMode
+    {
+        Shop_Buy,
+        Shop_Sell,
+        Inventory
+    }
+    internal class Item
+    {
+        [JsonInclude]
+        public int id { get; private set; }
+        [JsonInclude]
+        public int type { get; private set; }       //1이 무기, 2가 방어구
+        [JsonInclude]
+        public string name { get; private set; } = string.Empty;
+        [JsonInclude]
+        public string description { get; private set; } = string.Empty;
+        [JsonInclude]
+        public ItemStatus itemStatus { get; private set; }
+        [JsonInclude]
+        public int price { get; private set; }
+        [JsonInclude]
+        public bool isSold { get; private set; }
+        public Item() { }
+        public Item(int id, int type, string name, string description, ItemStatus itemStatus, int price)
+        {
+            this.id = id;
+            this.type = type;
+            this.name = name;
+            this.description = description;
+            this.itemStatus = itemStatus;
+            this.price = price;
+            this.isSold = false;
+        }
+        public void ViewInfo(ViewMode mode)
+        {
+            string stat = string.Empty;
+            string sellPrice = string.Empty;
+            if (itemStatus.atk != 0)
+            {
+                stat += $"{TextMessages.viewAttack} +{itemStatus.atk,-2} ";
+            }
+            if (itemStatus.def != 0)
+            {
+                stat += $"{TextMessages.viewDefence} +{itemStatus.def,-2} ";
+            }
+            if (itemStatus.health != 0)
+            {
+                stat += $"{TextMessages.viewHealth} +{itemStatus.health,-2} ";
+            }
+            switch (mode)
+            {
+                case ViewMode.Shop_Buy:
+                    sellPrice = isSold ? $"{TextMessages.viewSoldItemMent}" : $"{price} G";
+                    break;
+                case ViewMode.Shop_Sell:
+                    sellPrice = !isSold ? $"{TextMessages.viewSoldItemMent}" : $"{price} G";
+                    break;
+            }
+
+            string itemInfo = $"{PadingKorean(name, 18)} | {stat,-10} | {PadingKorean(description, 50)}";
+            if (mode != ViewMode.Inventory)
+            {
+                itemInfo += $" | {sellPrice,-5}";
+            }
+            Console.WriteLine(itemInfo);
+        }
+        public static string PadingKorean(string input, int width)
+        {
+            int len = 0;
+            foreach (char c in input)
+            {
+                len += (c >= 0xAC00 && c <= 0xD7A3) ? 2 : 1;
+            }
+            int pad = width - len;
+            if (pad > 0)
+                return input + new string(' ', pad);
+            return input;
+        }
+        public void ItemSold()
+        {
+            isSold = !isSold;
+            if (isSold)
+            {
+                PlayerManager.Instance.MainPlayer.AddItemToInvenTory(id, price);
+            }
+        }
+    }
+}
---
+++ TextConsoleRPG/ItemManager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TextConsoleRPG
+{
+    internal class ItemManager
+    {
+        private static ItemManager _instance;
+        public static ItemManager Instance => _instance ??= new ItemManager();
+        private ItemManager() { }
+
+        private Dictionary<int, Item> itemDict = new();
+        private List<Item> items = new();
+
+        public void LoadItems()
+        {
+            string json = File.ReadAllText(TextMessages.itemListFileFath);
+            items = JsonSerializer.Deserialize<List<Item>>(json);
+            foreach (var item in items)
+            {
+                itemDict[item.id] = item;
+            }
+        }
+        public void ShowItems(bool isBuyMode = false)
+        {
+            if (!isBuyMode)
+            {
+                foreach (var item in items)
+                {
+                    Console.Write("-");
+                    item.ViewInfo(ViewMode.Shop_Buy);
+                }
+            }
+            else
+            {
+                int i = 1;
+                foreach (var item in items)
+                {
+                    Console.Write("- {0} ", i++);
+                    item.ViewInfo(ViewMode.Shop_Buy);
+                }
+            }
+            Console.WriteLine();
+        }
+        public void BuyItem(int index)
+        {
+            Player p = PlayerManager.Instance.MainPlayer;
+            if (index > items.Count())
+            {
+                GameManager.Instance.WrongInput();
+                return;
+            }
+            Item item = items[--index];
+            if (item.isSold)
+            {
+                Console.WriteLine("이미 구매한 아이템입니다.(계속하려면 아무 키나 입력)");
+                Console.ReadKey();
+                return;
+            }
+            if (item.price > p.gold)
+            {
+                Console.WriteLine("Gold가 부족합니다.(계속하려면 아무 키나 입력)");
+                Console.ReadKey();
+                return;
+            }
+            else
+            {
+                item.ItemSold();
+                Console.WriteLine("구매를 완료했습니다.(계속하려면 아무 키나 입력)");
+                Console.ReadKey();
+                return;
+            }
+        }
+        public Item? GetItembyId(int id)
+        {
+            return itemDict.TryGetValue(id, out var item) ? item : null;
+        }
+        public void SaveData()
+        {
+            string jsonString = JsonSerializer.Serialize(items, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(TextMessages.itemListFileFath, jsonString);
+        }
+    }
+}
