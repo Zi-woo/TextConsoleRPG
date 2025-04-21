9ac8636400ddea57cc494e95841ef5653bcda92c
halonine9 <dlselhvxg@gmail.com>
2025-04-21 20:14:28 +0900

    공격 로직
    타겟 지정, 체력 감소, 생존 유무

--- TextConsoleRPG/Monster.cs
+++ TextConsoleRPG/Monster.cs
@@ -10,8 +10,10 @@ namespace TextConsoleRPG
     {
         public string Name { get; }
         public int Level { get; }
-        public int Hp { get; }
+        public int Hp { get; private set;}
         public int Atk { get; }
+
+        private List<Monster> MonsterList = new List<Monster>();
 
         public Monster(string name, int level, int hp, int atk)
         {
@@ -20,10 +22,20 @@ namespace TextConsoleRPG
             Hp = hp;
             Atk = atk;
         }
-
         public string MonsterInfoText()
         {
             return $"Lv.{Level} {Name} HP {Hp}";
+        }
+
+        
+        public bool AliveMonster() //몬스터 생존 확인 
+        {
+            return Hp > 0;
+        }
+        public void Damage(int damage)//몬스터 체력 감소
+        {
+            Hp -= damage;
+            if (Hp < 0) Hp = 0;
         }
     }
 }
