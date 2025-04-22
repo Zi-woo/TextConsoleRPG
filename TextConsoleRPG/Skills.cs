4ab87c5acc9d377a080919c5f5a26631308a56e2
Zi-woo <jiou64@naver.com>
2025-04-22 15:52:37 +0900

    feat: Add Skill
    스킬 시스템 추가 및 변수명 변경

---
+++ TextConsoleRPG/Skills.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextConsoleRPG
+{
+    class Skills
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int MpCost { get; set; }
+        public int Type { get; set; } // Type 1 : 대상지정필요 Type 2 : 대상지정불필요
+
+        public Action<Character, List<Monster>> Effect { get; set; }
+
+        public Skills(string name, string description, int mpCost,int type, Action<Character, List<Monster>> effect)
+        {
+            Name = name;
+            Description = description;
+            MpCost = mpCost;
+            Type = type;
+            Effect = effect;
+        }
+
+    }
+}
