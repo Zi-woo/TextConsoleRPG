56c884092424e34edf9d8326d1511884c89f445c
Zi-woo <jiou64@naver.com>
2025-04-23 14:45:55 +0900

    feat: Party Member
    파티원기능 추가

---
+++ TextConsoleRPG/PartyMember.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextConsoleRPG
+{
+    class PartyMember : Character
+    {
+        public PartyMember(int level, string name, string job, int atk, int def, int matk, int hp, int mp, List<Skills> learnedSkills)
+        : base(level, name, job, atk, def, matk, hp, mp, learnedSkills)
+        {
+
+        }
+
+    }
+}
